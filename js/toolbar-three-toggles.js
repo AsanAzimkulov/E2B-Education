fb2680f0e2174ca895df4a33f7387f945a5ba731
Asan Azimkulov <azimkulov40@gmail.com>
2022-09-01 21:53:35 +0600

    правки

--- js/toolbar-three-toggles.js
+++ js/toolbar-three-toggles.js
@@ -1,31 +1,17 @@
-$(document).ready(function () {
-  const items = document.querySelectorAll('.toolbar-three-column-toggles');
-
-  const createTabs = (element) => {
-    const handles = element.querySelectorAll('a');
-    const tabs = [...handles].map((handle) => document.getElementById(handle.getAttribute('href').slice(1)));
-
-    const activeHandle = element.querySelector('.toolbar-three-column-toggles__toggle--active');
-    showActiveTab(activeHandle);
-
-    handles.forEach(handle => handle.addEventListener('click', toggleTab))
-
-    function toggleTab(e) {
-      var position = $(document).scrollTop();
-      setTimeout(function () {
-        window.scrollTo(0, position);
-      }, 0);
-
-      handles.forEach(tab => tab.classList.remove('toolbar-three-column-toggles__toggle--active'));
-      e.target.classList.add('toolbar-three-column-toggles__toggle--active');
-      showActiveTab(e.target);
-    }
-    function showActiveTab(handle) {
-      tabs.forEach(tab => tab.style.display = 'none');
-      const index = [...handles].indexOf(handle);
-      [...tabs][index].style.display = 'block';
-    }
-  }
-
-  items.forEach(createTabs);
-});
+// $(document).ready(function () {
+//   const items = document.querySelectorAll('.toolbar-three-column-toggles');
+//   const allHandles = document.querySelectorAll('.toolbar-three-column-toggles a');
+//   items.forEach((item) => {
+//     const handles = item.querySelectorAll('a');
+//     handles.forEach((handle) => {
+//       handle.addEventListener('click', (e) => {
+//         const activeIndex = [...handles].indexOf(handle);
+//         allHandles.forEach(handle => handle.classList.remove('toolbar-three-column-toggles__toggle--active'));
+//         items.forEach((item) => {
+//           const handles = item.querySelectorAll('a');
+//           handles[activeIndex].classList.add('toolbar-three-column-toggles__toggle--active')
+//         })
+//       })
+//     })
+//   });
+// });
