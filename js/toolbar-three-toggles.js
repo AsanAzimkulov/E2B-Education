51b19574e1cfe4382dd999ee06ac0b7edd32478a
Asan Azimkulov <azimkulov40@gmail.com>
2022-09-03 12:18:00 +0600

    правки

--- js/toolbar-three-toggles.js
+++ js/toolbar-three-toggles.js
@@ -1,17 +1,18 @@
-// $(document).ready(function () {
-//   const items = document.querySelectorAll('.toolbar-three-column-toggles');
-//   const allHandles = document.querySelectorAll('.toolbar-three-column-toggles a');
-//   items.forEach((item) => {
-//     const handles = item.querySelectorAll('a');
-//     handles.forEach((handle) => {
-//       handle.addEventListener('click', (e) => {
-//         const activeIndex = [...handles].indexOf(handle);
-//         allHandles.forEach(handle => handle.classList.remove('toolbar-three-column-toggles__toggle--active'));
-//         items.forEach((item) => {
-//           const handles = item.querySelectorAll('a');
-//           handles[activeIndex].classList.add('toolbar-three-column-toggles__toggle--active')
-//         })
-//       })
-//     })
-//   });
-// });
+$(document).ready(function () {
+  const items = document.querySelectorAll('.toolbar-three-column-toggles');
+
+  items.forEach((item) => {
+    const handles = item.querySelectorAll('a');
+    const InitActiveHandle = [...handles].find(handle => handle.classList.contains('toolbar-three-column-toggles__toggle--active'));
+    handles.forEach((handle) => {
+      handle.addEventListener('click', (e) => {
+        handles.forEach(handle => handle.classList.remove('toolbar-three-column-toggles__toggle--active'));
+        handle.classList.add('toolbar-three-column-toggles__toggle--active');
+        setTimeout(() => {
+          handle.classList.remove('toolbar-three-column-toggles__toggle--active');
+          InitActiveHandle.classList.add('toolbar-three-column-toggles__toggle--active');
+        }, 1100);
+      })
+    })
+  });
+});
